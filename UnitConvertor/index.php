150e087832ccdaf5c12ca112533e1711c5b2e6e8
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-11-22 22:25:53 +0100

    Creating function, connecting to form

---
+++ UnitConvertor/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<?php
+
+function convertLength($value, $fromUnit, $toUnit)
+{
+    $conversionFactors = [];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $value = $_POST['value'];
+    $fromUnit = $_POST['from_unit'];
+    $toUnit = $_POST['to_unit'];
+
+    $result = convertLength($value, $fromUnit, $toUnit);
+    echo "<p>{$value} {$fromUnit} is equal to: {$result} {$toUnit}</p>";
+}
+
+?>
+
+
+<body>
+    <form method="post" action="">
+        <label for="value">Value:</label>
+        <input type="number" step="any" name="value" required>
+
+        <label for="from_unit">From:</label>
+        <select name="from_unit" required>
+            <option value="centimeters">Centimeter</option>
+            <option value="meters">Meter</option>
+        </select>
+
+        <label for="to_unit">To :</label>
+        <select name="to_unit" required>
+            <option value="centimeter">Centimeter</option>
+            <option value="meters">Meter</option>
+        </select>
+
+        <button type="submit">Convert</button>
+    </form>
+</body>
+
+</html>
